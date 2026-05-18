7f696ca4dd8a5d1bf7f061d6d17b5f2bcebb0c73
GoddderF <fyugetui@protonmail.com>
2026-05-18 17:03:11 +0800

    feat: 优化项目代码

--- src/main/java/com/hmdp/listener/SeckillVoucherListener.java
+++ src/main/java/com/hmdp/listener/SeckillVoucherListener.java
@@ -2,8 +2,7 @@ package com.hmdp.listener;
 
 import cn.hutool.json.JSONUtil;
 import com.hmdp.entity.VoucherOrder;
-import com.hmdp.service.impl.SeckillVoucherServiceImpl;
-import com.hmdp.service.impl.VoucherOrderServiceImpl;
+import com.hmdp.service.IVoucherOrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,16 +12,14 @@ import javax.annotation.Resource;
 import java.nio.charset.StandardCharsets;
 
 /**
- * 秒杀订单 MQ 消费者：保存订单并扣减库存
+ * 秒杀订单 MQ 消费者
  */
 @Component
 @Slf4j
 public class SeckillVoucherListener {
 
     @Resource
-    private SeckillVoucherServiceImpl seckillVoucherService;
-    @Resource
-    private VoucherOrderServiceImpl voucherOrderService;
+    private IVoucherOrderService voucherOrderService;
 
     @RabbitListener(queues = "QA")
     public void onNormalQueue(Message message) {
@@ -39,16 +36,6 @@ public class SeckillVoucherListener {
         log.info("{} 队列收到秒杀订单消息", queueLabel);
         VoucherOrder voucherOrder = JSONUtil.toBean(msg, VoucherOrder.class);
         log.info("订单内容: {}", voucherOrder);
-        voucherOrderService.save(voucherOrder);
-
-        Long voucherId = voucherOrder.getVoucherId();
-        boolean updated = seckillVoucherService.update()
-                .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId)
-                .gt("stock", 0)
-                .update();
-        if (!updated) {
-            log.warn("扣减秒杀库存未生效, voucherId={}, queue={}", voucherId, queueLabel);
-        }
+        voucherOrderService.handleVoucherOrder(voucherOrder);
     }
 }
